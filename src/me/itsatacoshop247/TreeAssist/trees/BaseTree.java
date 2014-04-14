74c9f7212d73df4329bc008b53c77b586fb04377
SLiPCoR <chris@slipcor.de>
2014-04-14 21:58:17 +0200

    v5.8.63 - fix the fact that new tools break. Thanks, TNTUP!

--- src/me/itsatacoshop247/TreeAssist/trees/BaseTree.java
+++ src/me/itsatacoshop247/TreeAssist/trees/BaseTree.java
@@ -308,7 +308,7 @@ public abstract class BaseTree {
 
 			if (player.getItemInHand().getDurability() > player.getItemInHand()
 					.getType().getMaxDurability() ||
-				player.getItemInHand().getDurability() < 1
+				player.getItemInHand().getDurability() < 0
 					&& Utils.isVanillaTool(player.getItemInHand())) {
 				player.setItemInHand(new ItemStack(Material.AIR));
 			}
